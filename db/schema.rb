78816671293ed4cb8b8fb89329c8dfd6fafa2d06
Mario Alvarez <mario@talentohq.com>
2025-02-24 15:51:29 +0100

    Add new schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_18_163457) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_19_094945) do
   create_table "accounts", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
   create_table "active_storage_attachments", force: :cascade do |t|
@@ -97,13 +98,19 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_18_163457) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email_address", null: false
+    t.string "email", null: false
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "account_id"
-    t.index ["account_id", "email_address"], name: "index_users_on_account_id_and_email_address", unique: true
-    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+    t.string "name"
+    t.string "username"
+    t.string "forget_token"
+    t.integer "role", default: 0
+    t.datetime "forget_expires_at"
+    t.boolean "active", default: true
+    t.index ["account_id", "email"], name: "index_users_on_account_id_and_email", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
