b2e2a3dc36b142762f5813b07e5e402760069504
Mario Alvarez <mario@talentohq.com>
2025-03-12 17:10:46 +0100

    Vaidation.

--- app/models/schedule_category.rb
+++ app/models/schedule_category.rb
@@ -2,4 +2,5 @@ class ScheduleCategory < ApplicationRecord
   belongs_to :account
 
   validates :name, presence: true
+  validates :colour, inclusion: { in: AVAILABLE_COLOURS }, allow_nil: true
 end
